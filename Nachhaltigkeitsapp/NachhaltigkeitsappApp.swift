de81de4b15fadadbb67f0fb95680eb6c99e9e752
Sarah <sarah.mesch1@iu-study.org>
2025-04-22 18:03:46 +0200

    Initial Commit

---
+++ Nachhaltigkeitsapp/NachhaltigkeitsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  NachhaltigkeitsappApp.swift
+//  Nachhaltigkeitsapp
+//
+//  Created by Manuel on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct NachhaltigkeitsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
